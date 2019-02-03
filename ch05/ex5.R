05664d4362fafc6ad4ba88d74be5e31db90c5ac1
(cdep)illabout <cdep.illabout@gmail.com>
2019-02-03 15:14:52 +0900

    Add exercise 5 for chapter 5.

--- ch05/ex5.R
+++ ch05/ex5.R
@@ -2,15 +2,44 @@ library(ISLR)
 library(MASS)
 library(class)
 
-set.seed(5)
+set.seed(2)
 
-fit <- glm(default ~ income + balance, data = Default, family = binomial)
+formula = default ~ income + balance + student
 
-probs <- predict(fit, type="response")
+default_length = length(Default[,1])
 
-pred <- rep("No", length(Default[,1]))
-pred[probs > .5] <- "Yes"
+######
+## Normal Logistic Regression on the whole set
+######
 
-glm_correct_percent <- mean(pred == Default$default)
+fit_everything <- glm(formula, data = Default, family = binomial)
 
+probs_everything <- predict(fit_everything, type="response")
 
+pred_everything <- rep("No", length(Default[,1]))
+pred_everything[probs_everything > .5] <- "Yes"
+
+glm_correct_percent_everything <- mean(pred_everything == Default$default)
+
+######
+## Normal Logistic Regression on just train set
+######
+
+train_set_indicies = sample(default_length, default_length/2)
+
+default_train_length = length(train_set_indicies)
+default_test_length = default_length - default_train_length
+
+fit_train_set <-
+  glm(formula, data = Default, family = binomial, subset = train_set_indicies)
+probs_test_set <-
+  predict(fit_train_set, newdata = Default[-train_set_indicies,],
+          type="response")
+
+pred_test_set <- rep("No", default_test_length)
+pred_test_set[probs_test_set > .5] <- "Yes"
+
+glm_correct_percent_test_set <- mean(pred_test_set == Default$default[-train_set_indicies])
+
+glm_correct_percent_everything
+glm_correct_percent_test_set
